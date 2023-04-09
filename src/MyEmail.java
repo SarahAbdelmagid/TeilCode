36e0048c32baef9cd10f4947ad3eb44261b719d2
SarahAbdelmagid <129907963+SarahAbdelmagid@users.noreply.github.com>
2023-04-09 23:28:10 +0300

    Update MyEmail.java

--- src/MyEmail.java
+++ src/MyEmail.java
@@ -90,7 +90,7 @@ public class Main {
 
     public static void sendMailTo(ArrayList<Member> members) {
         Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com"); // replace with your SMTP server host
+        props.put("mail.smtp.host", "smtp.gmail.com"); 
         props.put("mail.smtp.port", "587");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
